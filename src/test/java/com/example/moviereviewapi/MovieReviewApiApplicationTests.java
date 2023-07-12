ecdc5a7c7140c11b53e42dc3cdb21ff5d518636c
Praket Chavan <praket.chavan@gmail.com>
2023-07-12 22:13:08 +0100

    Initial Commit

---
+++ src/test/java/com/example/moviereviewapi/MovieReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviereviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
